02515d6bec60211fbc4e77fe98656c8a6934a7ff
nicolas grippi <nicolasgrippi96gmail.com>
2024-08-03 16:59:38 -0300

    Logo url

--- js/funciones.js
+++ js/funciones.js
@@ -1,16 +1,16 @@
 const camisetas = [
-    { id: 1, nombre: "Argentina", precio: 88000, imagen: "argentina.jpg"  },
-    { id: 2, nombre: "Argentina Titular", precio: 90000, imagen:"argentinatitular.jpg"  },
+    { id: 1, nombre: "Argentina", precio: 88000, imagen: "argentina.jpg" },
+    { id: 2, nombre: "Argentina Titular", precio: 90000, imagen: "argentinatitular.jpg" },
     { id: 3, nombre: "Argentina Entrenamiento", precio: 60000, imagen: "argentrenamiento.jpg" },
-    { id: 4, nombre: "Brasil", precio: 70000, imagen:"brasil.jpg" },
+    { id: 4, nombre: "Brasil", precio: 70000, imagen: "brasil.jpg" },
     { id: 5, nombre: "España", precio: 70000, imagen: "espana.jpg" },
-    { id: 6, nombre: "Italia", precio: 65000, imagen: "italia.jpg"},
+    { id: 6, nombre: "Italia", precio: 65000, imagen: "italia.jpg" },
     { id: 7, nombre: "Portugal", precio: 60000, imagen: "portugal.jpg" },
-    { id: 8, nombre: "Colombia", precio: 60000, imagen: "colombia.jpg" } 
+    { id: 8, nombre: "Colombia", precio: 60000, imagen: "colombia.jpg" }
 ]
 
 
-function agregarProducto(id) { 
+function agregarProducto(id) {
     const producto = camisetas.find(item => item.id == id);
     const carrito = cargarCarrito();
     carrito.push(producto);
@@ -22,10 +22,10 @@ function agregarProducto(id) {
         title: "El producto #" + id + " se ha agregado correctamente!",
         showConfirmButton: false,
         timer: 1500,
-        width:"440px",
+        width: "440px",
         heightAuto: false
-      });
-  
+    });
+
 }
 
 function eliminarProducto(id) {
@@ -41,20 +41,20 @@ function eliminarProducto(id) {
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
-         cancelButtonText: "No, cancelar",
+        cancelButtonText: "No, cancelar",
         confirmButtonText: "Si, eliminarlo"
-      }).then((result) => {
+    }).then((result) => {
         if (result.isConfirmed) {
-          Swal.fire({
-            title: "Eliminado",
-            text: "El producto ha sido eliminado correctamente!",
-            icon: "success"
-          });
+            Swal.fire({
+                title: "Eliminado",
+                text: "El producto ha sido eliminado correctamente!",
+                icon: "success"
+            });
         }
-      });
-    }
+    });
+}
 
- 
+
 
 function recorrerBotonCarrito() {
     let total = totalCamisetas();
@@ -85,10 +85,10 @@ function vaciarCarrito() {
         title: "El carrito se ha vaciado correctamente!",
         showConfirmButton: false,
         timer: 1500,
-        width:"440px",
+        width: "440px",
         heightAuto: false
-      });
-   
+    });
+
 }
 
 function cargarProducto() {
